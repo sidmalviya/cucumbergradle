46cad3c52c788e8b45e69d73b3eb69eb77222ed4
sidmalviya <68945057+sidmalviya@users.noreply.github.com>
2023-02-06 22:05:20 +0530

    first commit

---
+++ src/test/java/theinternet/TestSteps.java
@@ -0,0 +1,102 @@
+package theinternet;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.rules.Timeout;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class TestSteps {
+    private WebDriver driver;
+    static String TestString;
+
+    @Before
+    public void setUp() {
+
+        // ChromeDriver path on development machine, which is Windows
+        String OS = System.getProperty("os.name");
+        if (OS.startsWith("Mac")) {
+            System.setProperty("webdriver.chrome.driver",
+                    Paths.get("src/test/resources/chromedriver_win32/chromedriver.exe").toString());
+
+        }
+
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        }
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver!=null) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    @Given("Navigate to Page ForgotPassword")
+    public void navigateToPageForgotPassword() {
+        driver.navigate().to("https://the-internet.herokuapp.com/forgot_password");
+    }
+
+    @When("A User enters a valid email id")
+    public void aUserEntersAValidEmailId() {
+        driver.findElement(By.name("email")).sendKeys("valid@example.com");
+    }
+
+    @And("A User clicks on Retrieve password button")
+    public void aUserClicksOnRetrievePasswordButton() {
+        driver.findElement(By.id("form_submit")).click();
+    }
+
+    @When("A User enters a invalid email id")
+    public void aUserEntersAInvalidEmailId() {
+        driver.findElement(By.name("email")).sendKeys("invalid@");
+    }
+
+    @Then("Application shows that the email has been sent.")
+    public void applicationShowsThatTheEmailHasBeenSent() {
+        String actualMessage = driver.findElement(By.id("content")).getText();
+        assertThat(actualMessage.trim(), is("Your e-mail's been sent!"));
+    }
+
+   @Then("Application does not show that email has been sent.")
+    public void applicationDoesNotShowThatEmailHasBeenSent() {
+        String actualMessage = driver.findElement(By.id("content")).getText();
+        assertThat(actualMessage.trim(), not("Your e-mail's been sent!"));
+    }
+
+    @Given("A User Navigates to StatusCodes Page")
+    public void aUserNavigatesToStatusCodesPage() {
+        driver.navigate().to("https://the-internet.herokuapp.com/status_codes");
+    }
+
+    @When("A User Clicks on status Code {int}")
+    public void aUserClicksOnStatusCodeInput(Integer inputCode) {
+        driver.findElement(By.partialLinkText(inputCode.toString())).click();
+    }
+
+    @Then("Application displays the message {int}")
+    public void applicationDisplaysTheMessageOutputCode(Integer outputCode) {
+        String expectedMessage = "This page returned a "+outputCode.toString()+" status code.";
+        String actualMessage = driver.findElement(By.cssSelector("h3 + p")).getText();
+
+        assertThat(actualMessage, containsString(expectedMessage));
+    }
+}
